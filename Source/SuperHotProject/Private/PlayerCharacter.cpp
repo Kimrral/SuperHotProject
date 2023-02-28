e11524cb1e4ded5e6cf69c03e2cc5bde46966f81
Kimral <kimhhyunjjun@gmail.com>
2023-02-28 17:59:13 +0900

    Feat : Time Dilation
    Time Dilation added.

--- Source/SuperHotProject/Private/PlayerCharacter.cpp
+++ Source/SuperHotProject/Private/PlayerCharacter.cpp
@@ -1,6 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+#include "Engine/LocalPlayer.h"
+#include "EnhancedInputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "EnhancedInputSubsystems.h"
 #include "PlayerCharacter.h"
 
 // Sets default values
@@ -9,6 +13,9 @@ APlayerCharacter::APlayerCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	VRCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("VRCamera"));
+	VRCamera->SetupAttachment(RootComponent);
+	VRCamera->bUsePawnControlRotation = true;
 }
 
 // Called when the game starts or when spawned
@@ -16,7 +23,18 @@ void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 
-
+	// Enhanced Input 사용처리
+	auto PC = Cast<APlayerController>(GetWorld()->GetFirstPlayerController());
+	if (PC)
+	{
+		// LocalPlayer
+		auto localPlayer = PC->GetLocalPlayer();
+		auto subSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(localPlayer);
+		if (subSystem)
+		{
+			subSystem->AddMappingContext(IMC_Default, 0);
+		}
+	}
 	
 }
 
@@ -32,5 +50,46 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	auto InputSystem = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+	if (InputSystem)
+	{
+		//Binding
+		InputSystem->BindAction(IA_Move, ETriggerEvent::Triggered, this, &APlayerCharacter::Move);
+		InputSystem->BindAction(IA_Move, ETriggerEvent::Completed, this, &APlayerCharacter::MoveReleased);
+		InputSystem->BindAction(IA_Look, ETriggerEvent::Triggered, this, &APlayerCharacter::Turn);
+	}
+}
 
+void APlayerCharacter::Move(const FInputActionValue& Values)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Move"))
+		// 사용자의 입력에 따라 앞 , 뒤 , 좌, 우로 이동하고 싶다.
+		// 1. 사용자의 입력에 따라
+	FVector2D Axis = Values.Get<FVector2D>();
+	XMovement = Axis.X;
+	YMovement = Axis.Y;
+	AddMovementInput(GetActorForwardVector(), Axis.Y);
+	AddMovementInput(GetActorRightVector(), Axis.X);
+	// 2. 앞뒤좌우라는 방향이 필요
+	//FVector Dir(Axis.X, Axis.Y, 0);
+	// 3. 이동하고 싶다.
+	// P = P0 + vt
+	//FVector P0 = GetActorLocation();
+	//FVector vt = Dir * moveSpeed *GetWorld()->DeltaTimeSeconds;
+	//FVector P = P0 + vt;
+	//SetActorLocation(P);
+
+}
+
+void APlayerCharacter::Turn(const FInputActionValue& Values)
+{
+	FVector2D Axis = Values.Get<FVector2D>();
+	AddControllerYawInput(Axis.X);
+	AddControllerPitchInput(Axis.Y);
+}
+
+void APlayerCharacter::MoveReleased()
+{
+	XMovement = 0;
+	YMovement = 0;
 }
--- Source/SuperHotProject/Public/PlayerCharacter.h
+++ Source/SuperHotProject/Public/PlayerCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "PlayerCharacter.generated.h"
 
 UCLASS()
@@ -19,11 +20,38 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(VisibleAnywhere, Category = "VRCamera")
+	class UCameraComponent* VRCamera;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// 필요속성 : 이동속도, 입력액션, 입력매핑컨텍스트
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+		float moveSpeed = 500.0f;
+	// Input Mapping Context
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+		class UInputMappingContext* IMC_Default;
+	// Input Action for Move
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+		class UInputAction* IA_Move;
+	// 마우스 입력처리
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+		class UInputAction* IA_Look;
+	// 회전처리 함수
+	void Turn(const FInputActionValue& Values);
+	// 이동처리 함수
+	void Move(const FInputActionValue& Values);
+	void MoveReleased();
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+		float XMovement;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+		float YMovement;
+
+
 
 };
