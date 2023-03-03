d5c6108d4f2df21264c997fc7b36d14c21b92b29
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2023-03-03 14:55:10 +0900

    Create : Base Weapon
    Add : Weapon Assets
    
    Create Base Weapon C++ class and BP_BaseWeapon

---
+++ Source/SuperHotProject/Private/BaseWeapon.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseWeapon.h"
+#include "Components/BoxComponent.h"
+#include "TimerManager.h"
+
+// Sets default values
+ABaseWeapon::ABaseWeapon()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComp"));
+	BoxComp->SetBoxExtent(FVector(25.f, 10.f, 15.f));
+	SetRootComponent(BoxComp);
+
+	MeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetupAttachment(BoxComp);
+
+	MeshComp->SetRelativeRotation(FRotator(0, -90, 0));
+
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> gunMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/FPWeapon/Mesh/SK_FPGun.SK_FPGun'"));
+	if (gunMesh.Succeeded()) {
+		MeshComp->SetSkeletalMesh(gunMesh.Object);
+	}
+
+}
+
+// Called when the game starts or when spawned
+void ABaseWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABaseWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABaseWeapon::DoFire()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Fire"));
+}
+
+void ABaseWeapon::FirePressed()
+{
+	GetWorld()->GetTimerManager().SetTimer(FireTimerHandle, this, &ABaseWeapon::DoFire, FireRate, true);
+	DoFire();
+
+}
+
+void ABaseWeapon::FireRelease()
+{
+	GetWorld()->GetTimerManager().ClearTimer(FireTimerHandle);
+}
+
+
---
+++ Source/SuperHotProject/Public/BaseWeapon.h
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseWeapon.generated.h"
+
+UENUM(BlueprintType)
+enum class EWeaponState : uint8 {
+	Idle,       // 초기상태
+	Equipped,   // 장착상태
+	Pickup     // 그랩 할 수 있는 상태
+};
+
+UCLASS()
+class SUPERHOTPROJECT_API ABaseWeapon : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseWeapon();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+private:
+	UPROPERTY(EditAnywhere, Category = Settings)
+	float FireRate = 0.5f;
+
+	UPROPERTY(EditAnywhere, Category = WeaponState)
+	EWeaponState mState = EWeaponState::Idle;
+
+	FTimerHandle FireTimerHandle;
+
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		class UBoxComponent* BoxComp;
+
+	UPROPERTY(EditAnywhere, Category = Settgins)
+		class USkeletalMeshComponent* MeshComp;
+
+	UFUNCTION(BlueprintCallable)
+	virtual void DoFire();
+
+	UFUNCTION(BlueprintCallable)
+	virtual void FirePressed();
+
+	UFUNCTION(BlueprintCallable)
+	virtual void FireRelease();
+
+	
+	FORCEINLINE float GetFireRate() { return FireRate; }
+	FORCEINLINE void SetFireRate(float rate) { FireRate = rate <= 0 ? 0.5 : rate; }
+
+	FORCEINLINE EWeaponState GetWeaponState() { return mState; }
+	FORCEINLINE void SetWeaponState(EWeaponState state) { mState = state; }
+
+	FORCEINLINE FTimerHandle GetFireTimerHandle() { return FireTimerHandle; }
+	
+
+};
